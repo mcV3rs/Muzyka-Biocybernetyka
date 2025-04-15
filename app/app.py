d902e1c6bf6b22b410f0e4e1ce5060e37cc91568
Jakub Gurgul <mcVers.pl@gmail.com>
2025-04-15 10:09:14 +0200

    Add compare rhythm route and HTML for audio comparison feature

--- app/app.py
+++ app/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/compare-rythm')
+def compare_rythm():
+    return render_template('compare_rythm.html')
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
